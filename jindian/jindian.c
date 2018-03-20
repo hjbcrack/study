54fc96024b46b43fe772a728008cb658353a2a3c
matt <hjbcrack@126.com>
2018-03-21 00:12:57 +0800

    update jindian.c

--- jindian/jindian.c
+++ jindian/jindian.c
@@ -1,5 +1,5 @@
 
-#define A1_8
+#define A5_3
 
 #ifdef A1_1
 
@@ -377,8 +377,20 @@ bool checkReverseEqual(string s1, string s2) {
     return false;
 }
 
+// ref code
+bool checkReverseEqual1(string s1, string s2) {
+    // write code here
+    if(s1.size()==0 || s2.size()==0) //注意不要漏括号；
+        return false;
+    string str=s1+s1;
+    if(str.find(s2)==-1)
+        return false;
+    return true;
+}
+
 int main()
 {
+/*
     string str1("waterbottle");
     string str2("erbottlewat");
 
@@ -388,7 +400,16 @@ int main()
     string str4("worldhello ");
 
     cout << checkReverseEqual(str3, str4) << endl;
+*/
+    string str1("waterbottle");
+    string str2("erbottlewat");
 
+    cout << checkReverseEqual1(str1, str2) << endl;
+
+    string str3("Hello world");
+    string str4("worldhello ");
+
+    cout << checkReverseEqual1(str3, str4) << endl;
 
     return 0;
 }
@@ -396,6 +417,254 @@ int main()
 
 #endif
 
+#ifdef A2_2
 
+// 2.2 链表中倒数第k个结点 
+#endif
+
+#ifdef A2_3
+
+// 2.3 访问单个结点的删除 
+#endif
+
+#ifdef A1_8
+
+// 2.4 链表分割 
+/*
+编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
+给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。
+注意：分割以后保持原来的数据顺序不变。
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool create_linklist(ListNode *head, int length)
+{
+    ListNode *node = NULL;
+    ListNode *node1 = NULL;
+
+    head->val = -1;
+
+    node = new ListNode;
+    if (node == NULL) {
+        return false;
+    }
+    node->val = 0;
+    node->next = NULL;
+    head->next = node;
+
+    for (int i = 1; i < length; i++) {
+        node1 = new ListNode;
+        if (node1 == NULL) {
+            return false;
+        }
+
+        node1->val = (i%2) ? i+1 : i;
+        node1->next = NULL;
+        node->next = node1;
+        node = node1;
+    }
+
+    for (node = head->next; node != NULL; node = node->next) {
+        cout << node->val << " ";
+    }
+    cout << endl;
+
+    return true;
+}
+
+ListNode* partition(ListNode* pHead, int x)
+{
+    ListNode* node;
+
+    if (pHead == NULL) {
+        return NULL;
+    }
+
+    node = pHead;
+    while(pHead->next != NULL){
+        if (pHead->val == pHead->next->val) {
+            tmp_node = pHead->next;
+            pHead->next = pHead->next->next;
+            delete tmp_node;
+        } else {
+            pHead = pHead->next;
+        }
+    }
+
+    return node;
+}
+
+int main(){
+    int length = 10;
+    ListNode *head = new ListNode;
+    ListNode *node;
+
+    create_linklist(head, length);
+    node = head->next;
+    while(node != NULL) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+
+    node = deleteDuplication(head->next);
+    while(node != NULL) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+#endif
+
+#ifdef A5_1
+
+// 5.1 二进制插入
+/* 
+有两个32位整数n和m，请编写算法将m的二进制数位插入到n的二进制的第j到第i位,
+其中二进制的位数从低位数到高位且以0开始。
+给定两个数int n和int m，同时给定int j和int i，意义如题所述，请返回操作后的数，
+保证n的第j到第i位均为零，且m的二进制位数小于等于i-j+1。
+测试样例：
+1024，19，2，6
+返回：1100
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// MY CODE
+int binInsert(int n, int m, int j, int i)
+{
+    if (j > i) {
+		return n;
+	}
+
+	int index = 0;
+	int ij = 1;
+	while (index < (i-j)) {
+		ij = (ij << 1)+1;
+		index++;
+	}
+
+	return (n | ((m << j) & (ij << j)));
+}
+
+int main()
+{
+    cout << binInsert(1024, 19, 2, 6) << endl;
+ 
+    return 0;
+}
+
+#endif
+
+#ifdef A5_2
+// 5.2 二进制小数
+/* 
+有一个介于0和1之间的实数，类型为double，返回它的二进制表示。
+如果该数字无法精确地用32位以内的二进制表示，返回“Error”。
+
+给定一个double num，表示0到1的实数，请返回一个string，代表该数的二进制表示或者“Error”。
+测试样例：
+0.625
+返回：0.101
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// MY CODE
+string printBin(double num)
+{
+	string str = "0.";
+	double tmp = num;
+	int count = 0;
+	if (num > 1) {
+		return "Error";
+	}
+
+	while (1) {
+		tmp *= 2.0;
+		if (-0.00000001 < (tmp - 1.0) && (tmp - 1.0) < 0.00000001) {
+			str += "1";
+			break;
+		} else if (tmp > 1.0) {
+			tmp = tmp - 1.0;
+			str += "1";
+		} else {
+			str += "0";
+		}
+		count++;
+		if (count > 32) {
+			return "Error";
+		}
+	}
+
+	return str;
+}
+
+int main()
+{
+    cout << 0.625 << " " << printBin(0.625) << endl;
+ 	cout << 0.46502 << " " << printBin(0.46502) << endl;
+    return 0;
+}
+
+#endif
+
+
+#ifdef A5_3
+
+// 5.3 最接近的数
+/* 
+有一个正整数，请找出其二进制表示中1的个数相同、且大小最接近的那两个数。(一个略大，一个略小)
+给定正整数int x，请返回一个vector，代表所求的两个数（小的在前）。保证答案存在。
+测试样例：
+2
+返回：[1,4]
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// MY CODE
+vector<int> getCloseNumber(int x)
+{
+	vector<int> vec;
+
+    return vec;
+}
+
+int main()
+{
+	vector<int> vec;
+	vec = getCloseNumber(2);
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << "\t";
+	}
+	cout << endl;
+ 
+    return 0;
+}
+
+#endif
 
 
